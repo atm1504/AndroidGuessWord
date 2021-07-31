6f314d50cf37de2283e1405f7a97bb310a5213e9
Amartya Mondal <atm1504.in@gmail.com>
2021-07-31 12:31:41 +0530

    Add score fragment

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -1,11 +1,14 @@
 package com.example.android.guesstheword.screens.score
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -14,6 +17,10 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
 
 class ScoreFragment : Fragment() {
 
+    private lateinit var viewModelFactory: ScoreViewModelFactory
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var binding: ScoreFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,7 +28,7 @@ class ScoreFragment : Fragment() {
     ): View {
 
         // Inflate view and obtain an instance of the binding class.
-        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater,
             R.layout.score_fragment,
             container,
@@ -30,13 +37,35 @@ class ScoreFragment : Fragment() {
 
         // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.scoreText.text = scoreFragmentArgs.score.toString()
-        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+
+        viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ScoreViewModel::class.java)
+
+        viewModelObservers()
+
+        binding.playAgainButton.setOnClickListener {
+            viewModel.onPlayAgain()
+        }
 
         return binding.root
     }
 
+    fun viewModelObservers() {
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+            Log.d("ATM", "Score is $newScore")
+        })
+
+        viewModel.eventPLayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
+            if (playAgain) {
+                onPlayAgain()
+            }
+        })
+
+    }
+
     private fun onPlayAgain() {
         findNavController().navigate(ScoreFragmentDirections.actionRestart())
+        viewModel.onPlayAgainComplete()
     }
 }
--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.android.guesstheword.screens.score
 
-class ScoreViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel(val finalScore: Int) : ViewModel() {
+
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPLayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    init {
+        Log.d("ATM", "ScoreViewModel Init")
+        _score.value = finalScore
+    }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
+    }
 }
